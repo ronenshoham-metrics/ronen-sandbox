f6ebf97e5ff20b516c5f79f0d57f3b3a67804624
Ronen Shoham <ronen.shoham@metrics.ca>
2020-08-03 10:16:36 -0400

    More types Better names

--- waveform-sample/src/sub.v
+++ waveform-sample/src/sub.v
@@ -4,18 +4,25 @@ input clk;
 
 reg reset = 1;
 
-reg bit_two_values=0;
-reg bit_x_then_two_value;
-reg bit_4_values;
+reg bit_random=0;
+reg bit_random_reset;
+reg bit_special;
+reg bit_full=0;
+reg bit_full_reset=0;
 
-reg [7:0] byte_random;
-reg [7:0] byte_x_z;
+reg [7:0] byte_random = 8'h0;
+reg [7:0] byte_random_reset = 8'h0;
+reg [7:0] byte_special;
+reg [7:0] byte_full=8'h0;
+reg [7:0] byte_full_reset;
 
+
+reg [31:0] random = 32'h0;
 reg [31:0] counter  = 32'h0;
 reg [31:0] counter_reverse  = 32'h0;
 
 initial
-    #($urandom_range(10,500)*1ns) reset <= 0;
+    #($urandom_range(100,1000)*1ns) reset <= 0;
 
 always @(posedge clk)
     begin : COUNTERS
@@ -25,15 +32,27 @@ always @(posedge clk)
 
 always @(posedge clk)
     begin : BITS
-        bit_two_values <= $random();    
-        bit_4_values <= random_bit_four_values();
-        if (!reset) bit_x_then_two_value <= $random();
+        bit_random <= $random();    
+        bit_special <= random_bit_x_and_z();
+        bit_full <= random_bit_four_values();
+        if (!reset) 
+        begin
+            bit_random_reset <= $random();
+            bit_full_reset <= random_bit_four_values();
+        end
+
     end
     
 always @(posedge clk)
     begin : BYTES
         byte_random <= $random();   
-        byte_x_z <= random_byte_x_and_z();
+        byte_special <= random_byte_x_and_z();
+        byte_full <= random_byte_four_values();
+        if (!reset) 
+        begin
+            byte_random_reset <= $random();
+            byte_full_reset <= random_byte_four_values();
+        end
     end
 
 
@@ -46,6 +65,12 @@ function random_bit_four_values;
     endcase
 endfunction
 
+function random_bit_x_and_z;
+    case ($urandom_range(0,1))
+        0 : random_bit_x_and_z = 1'bx;
+        1 : random_bit_x_and_z = 1'bz;
+    endcase
+endfunction
 
 function [3:0] random_nible_x_and_z;
     reg [2:0] data = $random();
@@ -59,6 +84,14 @@ endfunction
 
 function random_byte_x_and_z;
     random_byte_x_and_z = {random_nible_x_and_z(), random_nible_x_and_z()};
+endfunction
+
+function [3:0] random_byte_four_values;
+    reg [2:0] data = $random();
+    case ($urandom_range(0,10))
+        0 : random_byte_four_values = 4'hx; //Full X
+        default: random_byte_four_values = $random();
+    endcase
 endfunction
 
 
