5f8c608999966ec4c7e2b6ce7914532de763aea9
Ronen Shoham <ronen.shoham@metrics.ca>
2020-08-03 10:01:59 -0400

    Additional X and Z signals

--- waveform-sample/src/sub.v
+++ waveform-sample/src/sub.v
@@ -9,6 +9,7 @@ reg bit_x_then_two_value;
 reg bit_4_values;
 
 reg [7:0] byte_random;
+reg [7:0] byte_x_z;
 
 reg [31:0] counter  = 32'h0;
 reg [31:0] counter_reverse  = 32'h0;
@@ -25,23 +26,40 @@ always @(posedge clk)
 always @(posedge clk)
     begin : BITS
         bit_two_values <= $random();    
-        bit_4_values <= four_values();
+        bit_4_values <= random_bit_four_values();
         if (!reset) bit_x_then_two_value <= $random();
     end
     
 always @(posedge clk)
     begin : BYTES
-        byte_random <= $random();    
+        byte_random <= $random();   
+        byte_x_z <= random_byte_x_and_z();
     end
 
 
-function four_values;
+function random_bit_four_values;
     case ($urandom_range(0,3))
-        0 : four_values = 1'b0;
-        1 : four_values = 1'b1;
-        2 : four_values = 1'bx;
-        3 : four_values = 1'bz;
+        0 : random_bit_four_values = 1'b0;
+        1 : random_bit_four_values = 1'b1;
+        2 : random_bit_four_values = 1'bx;
+        3 : random_bit_four_values = 1'bz;
     endcase
 endfunction
+
+
+function [3:0] random_nible_x_and_z;
+    reg [2:0] data = $random();
+    case ($urandom_range(0,3))
+        0 : random_nible_x_and_z = 4'hx; //Full X
+        1 : random_nible_x_and_z = {data,1'bx}; // Partial X
+        2 : random_nible_x_and_z = 4'hz; // Full Z
+        3 : random_nible_x_and_z = {data,1'bz}; // Partial Z
+    endcase
+endfunction
+
+function random_byte_x_and_z;
+    random_byte_x_and_z = {random_nible_x_and_z(), random_nible_x_and_z()};
+endfunction
+
 
 endmodule
