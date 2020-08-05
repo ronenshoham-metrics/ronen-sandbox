a9a51b42fe5ff5a22ae623f88d32b2ad76371fbb
Ronen Shoham <ronen.shoham@metrics.ca>
2020-08-05 06:01:33 -0400

    Waveform directory name change

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd run
 pwd
-dsim -F ../build/repo/waveform-sample/scripts/build-debug.f
-dsim -F ../build/repo/waveform-sample/scripts/run-debug.f
+dsim -F ../build/repo/waveform/scripts/build-debug.f
+dsim -F ../build/repo/waveform/scripts/run-debug.f
 #ls -allh *
 
 dsim -F ../build/repo/logfile/scripts/build.f
